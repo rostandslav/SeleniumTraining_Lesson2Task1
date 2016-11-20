37c47346a159f05ee83f828c4a87fe32d17ad429
Rostislav <rostandslav@yandex.ru>
2016-11-21 00:11:31 +0500

    инициализация репо и добавление файлов

---
+++ SimpleTest/SimpleTestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SimpleTest
+{
+    [TestClass]
+    public class SimpleTestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.yandex.ru/";
+            driver.Close();
+        }
+    }
+}
